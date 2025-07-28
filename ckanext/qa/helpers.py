d011fb6905a26c34c0674c0f91e2e419f223904e
Momin Imtiaz <mominimtiaz0786@gmail.com>
2025-07-28 19:20:12 +0500

    fixes

--- ckanext/qa/helpers.py
+++ ckanext/qa/helpers.py
@@ -21,11 +21,13 @@ def qa_openness_stars_resource_html(resource):
 
 def qa_openness_stars_dataset_html(dataset):
     qa = dataset.get('qa')
-    if not qa:
-        return tk.literal('<!-- No qa info for this dataset -->')
-    if not isinstance(qa, dict):
-        return tk.literal('<!-- QA info was of the wrong type -->')
-    extra_vars = copy.deepcopy(qa)
+    if qa and isinstance(qa, dict):
+        extra_vars = copy.deepcopy(qa)
+    else:
+        extra_vars = {
+            'openness_score': None,
+            'openness_score_reason': tk._('Not calculated yet.'),
+        }
     return tk.literal(
         tk.render('qa/openness_stars_brief.html',
-                  extra_vars=extra_vars))
+                extra_vars=extra_vars))
